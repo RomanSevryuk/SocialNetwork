75af1a91737f10c7a76821473d038f297fa6aebb
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-10-16 02:42:30 +0300

    74 - componentDidUpdate

--- src/componets/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/componets/Profile/ProfileInfo/ProfileStatus.tsx
@@ -27,6 +27,14 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
         this.setState({status: e.currentTarget.value})
     }
 
+    componentDidUpdate(prevProps: Readonly<ProfileStatusPropsType>, prevState: Readonly<{}>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (<>
                 {!this.state.editMode ?
