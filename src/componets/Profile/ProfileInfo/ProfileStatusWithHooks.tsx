6c11dd9e82948bc46b8442ff850fa7429ee780dc
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-11-08 00:28:44 +0300

    refactor(ProfileStatus) used hooks: useEffect

--- src/componets/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/componets/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type ProfileStatusWithHooksPropsType = {
     status: string
@@ -9,6 +9,11 @@ export const ProfileStatusWithHooks = (props: ProfileStatusWithHooksPropsType) =
 
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        debugger
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => setEditMode(true)
     const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => setStatus(e.currentTarget.value)
