0df40f6b860361531cbdc2c3f2033a8fac5eda68
Roman Sevryuk <Sevryuk.Roman@outlook.com>
2023-11-14 01:37:37 +0300

    94 - React.lazy, React.Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,22 @@ import {BrowserRouter, Route} from "react-router-dom";
 import {News} from "./componets/News/News";
 import {Music} from "./componets/Music/Music";
 import {Settings} from "./componets/Settings/Settings";
-import {DialogsContainer} from "./componets/Dialogs/DialogsContainer";
+/*import {DialogsContainer} from "./componets/Dialogs/DialogsContainer";*/
 import {UsersContainer} from "./componets/Users/UsersContainer";
 import {HeaderContainer} from "./componets/Header/HeaderContainerClass";
 import {LoginPageContainer} from "./Login/Login";
-import ProfileClassContainer from "./componets/Profile/ProfileClassContainer";
+//import ProfileClassContainer from "./componets/Profile/ProfileClassContainer";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {setInitializedAppTC} from "./redux/app-reducer";
 import {AppRootStateType} from "./redux/redux-store";
 import {Preloader} from "./componets/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./componets/Dialogs/DialogsContainer')
+    .then(({DialogsContainer}) => ({default: DialogsContainer})));
+const ProfileClassContainer = React.lazy(() => import('./componets/Profile/ProfileClassContainer'));
+
 
 type AppPropsType = {
     setInitializedAppTC: () => void
@@ -40,8 +46,9 @@ export class App extends React.Component<AppPropsType> {
                     <HeaderContainer/>
                     <Navbar/>
                     <div className='appWrapperContent'>
-                        <Route path='/Dialogs' render={() => <DialogsContainer/>}/>
-                        <Route path='/Profile/:userID?' render={() => <ProfileClassContainer/>}/>
+                        <Route path='/Dialogs' render={withSuspense(DialogsContainer)}/>
+                        {/*<Route path='/Profile/:userID?' render={() => <ProfileClassContainer/>}/>*/}
+                        <Route path='/Profile/:userID?' render={withSuspense(ProfileClassContainer)}/>
                         <Route path='/News' component={News}/>
                         <Route path='/Music' component={Music}/>
                         <Route path='/Settings' component={Settings}/>
